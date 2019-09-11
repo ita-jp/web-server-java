5f2627910c165ead2835c8dec4752f48f6c62268
nagaita <tn00gm@gmail.com>
2019-09-11 09:38:15 +0900

    Read string directly from InputStream without using any decorator class.

--- src/main/java/com/example/web/server/java/HttpRequest.java
+++ src/main/java/com/example/web/server/java/HttpRequest.java
@@ -3,10 +3,8 @@ package com.example.web.server.java;
 import lombok.Getter;
 import lombok.val;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 
 public class HttpRequest {
 
@@ -16,14 +14,17 @@ public class HttpRequest {
     private String path;
 
     public HttpRequest(InputStream inputStream) throws IOException {
-        val bufferedInputStream = new BufferedReader(new InputStreamReader(inputStream));
-        val iter = bufferedInputStream.lines().iterator();
-        if (!iter.hasNext()) {
-            return;
+        int ch;
+        val sb = new StringBuilder();
+        while ((ch = inputStream.read()) != -1) {
+            if (ch == '\r') {
+                val requestLine = sb.toString().split(" +");
+                method = requestLine[0];
+                path = requestLine[1];
+                break;
+            }
+            sb.append((char) ch);
         }
-        val requestLine = iter.next().split(" +");
-        method = requestLine[0];
-        path = requestLine[1];
     }
 
 }
