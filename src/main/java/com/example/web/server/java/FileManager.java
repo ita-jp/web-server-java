328f3a6484864699bb3399e8bb77db3748f142fa
nagaita <tn00gm@gmail.com>
2019-09-13 08:55:05 +0900

    feat. Implement FileManager

--- src/main/java/com/example/web/server/java/FileManager.java
+++ src/main/java/com/example/web/server/java/FileManager.java
@@ -1,16 +1,23 @@
 package com.example.web.server.java;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Optional;
 
 public class FileManager {
 
     public Optional<BufferedReader> createBufferedReader(String path) {
-        return null;
+        try {
+            return Optional.of(Files.newBufferedReader(Paths.get(path)));
+        } catch (IOException e) {
+            return Optional.empty();
+        }
     }
 
     public Optional<BufferedReader> createBufferedReaderFor404() {
-        return null;
+        return createBufferedReader("./src/main/resources/404.html");
     }
 
 }
